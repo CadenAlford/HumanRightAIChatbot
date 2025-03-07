1b9399f482c0beeb722506031f0a968360a404aa
CadenAlford <31745412+CadenAlford@users.noreply.github.com>
2025-03-07 14:53:29 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
 export const OWNER_NAME: string = `Caden Alford`;
-export const OWNER_DESCRIPTION: string = `Computer Science Student at the University of North Carolina at Chape Hill`;
+export const OWNER_DESCRIPTION: string = `Computer Science Student at the University of North Carolina at Chapel Hill`;
 
 export const AI_NAME: string = `Amani`;
 export const AI_TONE: string = `Friendly and Professional.`;
