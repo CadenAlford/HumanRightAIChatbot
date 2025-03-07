d94349092e3d186692b92c12a7f42c4505b8df2b
CadenAlford <31745412+CadenAlford@users.noreply.github.com>
2025-03-07 14:54:56 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,7 +1,7 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `Chat with Amani`;
-export const MESSAGE_PLACEHOLDER: string = `Hello, I'm Amani, Your personal Human Rights Assistant! I'm here to help with your questions about Human Rights!`;
+export const MESSAGE_PLACEHOLDER: string = `What are Human Rights?`;
 export const FOOTER_MESSAGE: string = `Customized AI by Caden Alford @ UNC`;
 export const CLEAR_BUTTON_TEXT: string = `Clear`;
 export const PAGE_TITLE: string = `Amani`;
