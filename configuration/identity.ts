cfc821f012808c732b5b1b1b34ec42fbf2afd679
CadenAlford <31745412+CadenAlford@users.noreply.github.com>
2025-03-06 19:51:21 -0500

    Update identity.ts
    Updated AI_Tone

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `Caden Alford`;
 export const OWNER_DESCRIPTION: string = `Computer Science Student at the University of North Carolina at Chape Hill`;
 
 export const AI_NAME: string = `Amani`;
-export const AI_TONE: string = `[AI TONE]`;
+export const AI_TONE: string = `Friendly and Professional.`;
 export const AI_ROLE: string = `[AI ROLE]`;
