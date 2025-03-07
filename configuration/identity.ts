8bdbe738cccf351c8c022ee8d285d816dbe365a5
CadenAlford <31745412+CadenAlford@users.noreply.github.com>
2025-03-06 19:34:41 -0500

    Update identity.ts
    Updated Owner Name and Description 
    Updated AI_Name

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `Caden Alford`;
+export const OWNER_DESCRIPTION: string = `Computer Science Student at the University of North Carolina at Chape Hill`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `Amani`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
