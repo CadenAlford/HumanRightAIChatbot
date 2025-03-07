cafa64587d0b1872575e9571c5ae1659e5ce82c7
CadenAlford <31745412+CadenAlford@users.noreply.github.com>
2025-03-06 20:05:36 -0500

    Update identity.ts
    updated AI_ROLE

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -3,4 +3,4 @@ export const OWNER_DESCRIPTION: string = `Computer Science Student at the Univer
 
 export const AI_NAME: string = `Amani`;
 export const AI_TONE: string = `Friendly and Professional.`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_ROLE: string = `Human Rights Expert and Advocate`;
