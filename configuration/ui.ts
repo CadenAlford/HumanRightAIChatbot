9079e1dd27a2b13887cc3b6ac5a1861380a5c098
CadenAlford <31745412+CadenAlford@users.noreply.github.com>
2025-03-06 19:39:06 -0500

    Update ui.ts
    - Updated Page Description

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -5,6 +5,6 @@ export const MESSAGE_PLACEHOLDER: string = `Hello, I'm Amani, Your personal Huma
 export const FOOTER_MESSAGE: string = `Customized AI by Caden Alford @ UNC`;
 export const CLEAR_BUTTON_TEXT: string = `Clear`;
 export const PAGE_TITLE: string = `Amani`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const PAGE_DESCRIPTION: string = `Hello, I'm ${AI_NAME}, Your personal Human Rights Assistant! I'm here to help with your questions about Human Rights!.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
