8fb3ef9aa628f5f155eae4eeecd138e5870b7020
CadenAlford <31745412+CadenAlford@users.noreply.github.com>
2025-03-06 19:30:30 -0500

    Update ui.ts
    -Message Placeholder updated. 
    - Page Title Updated.

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `Chat with Amani`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
+export const MESSAGE_PLACEHOLDER: string = `Hello, I'm Amani, Your personal Human Rights Assistant! I'm here to help with your questions about Human Rights!`;
 export const FOOTER_MESSAGE: string = `Customized AI by Caden Alford @ UNC`;
 export const CLEAR_BUTTON_TEXT: string = `Clear`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const PAGE_TITLE: string = `Amani`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
